9201c991fa3280b05a8d064f09ab3147ffed5bfa
Juan Augusto <107640040+JuanAug@users.noreply.github.com>
2022-07-13 14:35:58 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+function darkblue() {
+    return document.getElementById("main").className=("dark-blue")
+}
+
+function darkpink() {
+    return document.getElementById("main").className=("dark-pink")
+}
+
+function pink() {
+    return document.getElementById("main").className=("pink")
+}
+
+function blue() {
+    return document.getElementById("main").className=("blue")
+}
